e2c17eb5c3c2ebe6d746b1b823b67a009518231c
Tatiana Shevchenko <125043008+Skitalica@users.noreply.github.com>
2023-04-04 14:47:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,7 +7,13 @@ from scipy.stats import norm
 chat_id = 277684942 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
+    '''n = len(x)
     lower = np.min(x) / (1 - np.sqrt(1 - p))
     upper = np.max(x) / (1 - np.sqrt(1 - p))
     return (lower, upper)
+    это на 1 балл вышло'''
+    alpha = 1 - p
+    loc = x.mean()
+    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
+    return loc - scale * norm.ppf(1 - alpha / 2), \
+           loc - scale * norm.ppf(alpha / 2)
