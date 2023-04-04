0c74f86d983cb2624b98d702ed5b0c0eaf19ce8e
Tatiana Shevchenko <125043008+Skitalica@users.noreply.github.com>
2023-04-04 21:27:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import t 
 from scipy.stats import norm
 
 
-chat_id = 277684942 # Ваш chat ID, не меняйте название переменной
+chat_id = 277684942 
 
 def solution(p: float, x: np.array) -> tuple:
+    a = 0.047
+    alpha = 1 - p
     n = len(x)
-    variance = (((2 * np.mean(x) - 0.065) - 0.065) ** 2) / 12
-    z = norm.ppf(1 - p / 2)
-    interval = ((2 * np.mean(x) - 0.065) - z * np.sqrt(variance / n), (2 * np.mean(x) - 0.065) + z * np.sqrt(variance / n))
+    b = np.max(x)
+    interval = b, b + t.ppf (1 - alpha, n - 1)* np.sqrt( (b - a) ** 2 /(12 * n))
     return interval
