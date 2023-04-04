f6042f5361f016d6c1c1a6b999ea5eb60ef25238
Tatiana Shevchenko <125043008+Skitalica@users.noreply.github.com>
2023-04-04 19:28:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,8 +7,8 @@ from scipy.stats import norm
 chat_id = 277684942 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    n = len(x)
+    variance = (((2 * np.mean(x) - 0.065) - 0.065) ** 2) / 12
+    z = norm.ppf(1 - p / 2)
+    interval = ((2 * np.mean(x) - 0.065) - z * np.sqrt(variance / n), (2 * np.mean(x) - 0.065) + z * np.sqrt(variance / n))
+    return interval
