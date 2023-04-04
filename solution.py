e23f41eb661efc09e18d47ba32641b5c30f3a80a
Tatiana Shevchenko <125043008+Skitalica@users.noreply.github.com>
2023-04-04 21:34:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,9 +7,9 @@ from scipy.stats import norm
 chat_id = 277684942 
 
 def solution(p: float, x: np.array) -> tuple:
-    a = 0.047
+    a = 0.065
     alpha = 1 - p
     n = len(x)
     b = np.max(x)
-    interval = b, b + t.ppf (1 - alpha, n - 1)* np.sqrt( (b - a) ** 2 /(12 * n))
+    interval = (b, b + t.ppf (1 - alpha, n - 1) * np.sqrt( (b - a) ** 2 /(12 * n)))
     return interval
